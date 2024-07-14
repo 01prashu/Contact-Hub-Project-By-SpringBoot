c9cb605566fb11309a0333796263068c9cdf9f9d
Prashant Netke <netkeprashant2324@gmail.com>
2024-07-14 23:24:02 +0530

    commit 14July

---
+++ src/main/java/com/contact/test/ContactManager1Application.java
@@ -0,0 +1,13 @@
+package com.contact.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManager1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManager1Application.class, args);
+	}
+
+}
